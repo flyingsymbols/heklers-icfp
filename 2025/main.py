d9429d043cdb44cc0533c9b97ad7028877342d49
Justin Szejner <jszejner@gmail.com>
2025-09-06 14:27:49 -0400

    making api a file

--- 2025/main.py
+++ 2025/main.py
@@ -1,41 +1,12 @@
 #!/usr/bin/env python3
 
-import requests
-
-
-class LibraryApi:
-    def __init__(self, id):
-        self.base_url = "https://31pwr5t6ij.execute-api.eu-west-2.amazonaws.com/"
-        self.id = id
-
-    def register(self, team_name, preferred_language, email):
-        return requests.post(
-            f"{self.base_url}/register",
-            json={
-                "name": team_name,
-                "pl": preferred_language,
-                "email": email,
-            },
-        ).json()
-
-    def select(self, problem_name):
-        return requests.post(
-            f"{self.base_url}/select", json={"id": self.id, "problemName": problem_name}
-        ).json()
-
-    def explore(self, plans):
-        return requests.post(
-            f"{self.base_url}/explore", json={"id".self.id, "plans", plans}
-        ).json()
-
-    def guess(self, map):
-        return requests.post(
-            f"{self.base_url}/guess", json={"id": self.id, "map": map}
-        ).json()
+from LibraryApi import LibraryApi
 
 
 def main():
-    pass
+    lib_api = LibraryApi()
+    lib_api.select("probatio")
+    print(lib_api.explore(plans=["123"]))
 
 
 if __name__ == "__main__":
